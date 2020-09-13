dd00b5886939b36091699173420ca9d62734e4a7
vertig0d <gani.zero1@outlook.com>
2020-09-13 20:44:40 +0530

    Combine 2 tables

---
+++ #175CombineTwoTables.sql
@@ -0,0 +1,27 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+-- FirstName, LastName, City, State
+
+/* Write your T-SQL query statement below */
+select p.FirstName, p.LastName, a.City, a.State from Person as p left outer join Address as a on p.PersonId = a.PersonId
